366d815cf9c1f9ca0ed3ce48fde7fdd5d8a321b1
Michael Melikov <coutaq@gmail.com>
2020-05-20 17:53:21 +0300

    Add project files.

---
+++ LabNine/BadList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabNine
+{
+    internal class BadList
+    {
+        internal DoubleNode head;
+    }
+    internal class DoubleNode
+    {
+        internal int data;
+        internal DoubleNode prev;
+        internal DoubleNode next;
+        public DoubleNode(int d)
+        {
+            data = d;
+            prev = null;
+            next = null;
+        }
+    }
+}
---
+++ LabNine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabNine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<GeographicalUnit> countries = new List<GeographicalUnit>();
+            List<LogEntry> log = new List<LogEntry>();
+            ConsoleApp.Execute(countries, log);
+        }
+    }
+}
